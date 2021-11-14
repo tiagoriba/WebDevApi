2f8e72ed239b2c21fba68521ec8acd8fd65c25d5
Tiago Ribeiro <tiago.ribeiro0@outlook.com>
2021-11-14 23:57:30 +0000

    last commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+const envIsHeroku = PORT != 8000 ? true :false
 const puppeteer = require('puppeteer');
 const express = require('express');
 const axios = require('axios');
@@ -7,9 +8,13 @@ const cheerio = require('cheerio');
  
 const app = express();
 
-async function configureBrowser() {
+const pcdiga = 'https://www.pcdiga.com/#/embedded/query=';
+const pccomponentes = 'https://www.pccomponentes.pt/buscar/?query=';
+const chipsite = 'https://www.chipsite.pt/procurar?controller=search&s=';
+
+async function configureBrowser( url) {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: envIsHeroku ? true : false,
         defaultViewport: null,
         args : [
             '--no-sandbox',
@@ -17,42 +22,61 @@ async function configureBrowser() {
           ]
       });
     const page = await browser.newPage();
-    await page.goto('https://www.pcdiga.com/#/embedded/query=asus', {
+    await page.goto(url, {
         waitUntil: 'networkidle0',
-      });
+    });
     return page;
+
 }
 
 const searchResults = [];
 
-app.get('/', async (req, res) => {
-    let page = await configureBrowser();
+async function getAllFromPcComp(search){
+    let page = await configureBrowser(pccomponentes + search);
 
     let html = await page.evaluate(() => document.body.innerHTML);
-    
     const $ = cheerio.load(html);
-    $('div[class="df-card"]',html).each(function (index, element) {
-        console.log($(element).html());
-        var title = $(this).find('.df-card__title').text();
-        searchResults.push(title);
-    }) 
-    res.json(searchResults);
-    //res.json('Welcome');
-    /* axios.get('https://www.pcdiga.com').then((response) => {
-        const html = response.data;
-        console.log(html);
-    }) */
-    /* cloudscraper.get('https://www.pccomponentes.pt/buscar/?query=monitor').then((body) => {
-        res.json(body);
-        const $ = cheerio.load(body);
-        //console.log($.html());
+    $('a[data-list="search results"]',html).each(function (index, element) {
+        var title = $(this).data('name');
+        searchResults.push({
+            title: $(this).data('name'),
+            price: $(this).data('price'),
+            link:$(this).attr('href')
+        });
         
-        /* $('[data-list="search results"]').each(() => {
-            console.log( 'dwqdqw');
-        }) 
-       
-    }); 
- */
+    }) 
+}
+
+async function getAllFromChipSite(search){
+    let page = await configureBrowser(chipsite + search);
+    let html = await page.evaluate(() => document.body.innerHTML);
+    const $ = cheerio.load(html);
+    $('article',html).each(function (index, element) {
+        var ehref = $(this).find('.product-name > a');
+        searchResults.push(
+            {
+                title: ehref.text(),
+                price: ehref.attr('href'),
+                link:$(this).find('[class="price product-price"]').text()
+            }
+            );
+        
+    }) 
+}
+app.get('/', async (req, res) => {
+    res.json({
+        comousar: 'Para procurar por um componente basta no url adicionar o que deseja procurar Ex. https://webdevapi30000100.herokuapp.com/Monitor Asus',
+        help: 'Para aparecer de mais sites como por ex. pccomponentes, usar no computador git: https://github.com/tiagoriba/WebDevApi'
+    });
+});
+app.get('/:search', async (req, res) => {
+    let search = req.params.search;
+    await getAllFromChipSite(search);
+    if (!envIsHeroku) {
+        await getAllFromPcComp(search);
+    }
+    res.json(searchResults);
+    
 })
 
-app.listen(process.env.PORT || 8000)
+app.listen(PORT)
